4e14cca237622ec6154786b60e011939930ccc7c
Evamy <antriksh5235@gmail.com>
2016-03-05 19:57:45 +0530

    Parameter updates, but not all

---
+++ Assignment1/parameter_update.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+
+"""
+Parameter update has been under great scrutiny just because of the fact that
+it can be more and better optimised.
+
+Research is still going on and a new and better formula for Parameter updates is always welcome.
+
+"""
+
+
+def gradient_descent():
+	"""
+	TODO
+		This is the simplest of them all and performs the worst.
+
+	"""
+
+	return grad
+
+def momentum():
+	"""
+	TODO
+		This is a simple upgradation of gradient descent and performs much much faster than
+		the simple gradient descent. This one uses a velocity and momentum to improve the
+		performance of the simple gradient descent
+
+	"""
+
+	return grad
+
+def adagrad():
+	"""
+	TODO
+
+	"""
